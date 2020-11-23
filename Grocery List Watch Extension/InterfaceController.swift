94f83b9f4194371784fdd19552ca20a6d68fc086
Itunu Raimi <chavitunu@gmail.com>
2020-11-23 10:15:00 +0100

    worked with multiple screens on apple watch

--- Grocery List Watch Extension/InterfaceController.swift
+++ Grocery List Watch Extension/InterfaceController.swift
@@ -9,11 +9,13 @@ import WatchKit
 import Foundation
 
 class InterfaceController: WKInterfaceController {
-    @IBOutlet var myButton: WKInterfaceButton!
-    @IBOutlet var textLabel: WKInterfaceLabel!
+    @IBOutlet var button1: WKInterfaceButton!
+    @IBOutlet var button2: WKInterfaceButton!
+    @IBOutlet var button3: WKInterfaceButton!
+    
     
     override func awake(withContext context: Any?) {
-//        setupTableView()
+        //        setupTableView()
     }
     
     override func willActivate() {
@@ -24,26 +26,39 @@ class InterfaceController: WKInterfaceController {
         
     }
     
-    @IBAction func buttonTapped() {
-        self.presentTextInputController(withSuggestions: nil, allowedInputMode: .allowEmoji) { (results) in
-            guard let results = results else {return}
-            DispatchQueue.main.async {
-                self.dismissTextInputController()
-                self.textLabel.setText(results[0] as? String)
-            }
-        }
+    @IBAction func button1Tapped() {
+        self.pushController(withName: "first", context: nil)
     }
     
-//    func setupTableView() {
-//        let fruits = ["Apples", "Oranges", "Cherries", "Mangos", "Berries"]
-//        myTable.setNumberOfRows(fruits.count, withRowType: "cell")
-//        var x = 0
-//        for fruit in fruits {
-//            let row = myTable.rowController(at: x) as? RowController
-//            row?.myLabel.setText(fruit)
-//            x += 1
+    @IBAction func button2Tapped() {
+        self.pushController(withName: "second", context: nil)
+    }
+    
+    @IBAction func button3Tapped() {
+        self.pushController(withName: "third", context: nil)
+    }
+    
+    //    func setupTableView() {
+    //        let fruits = ["Apples", "Oranges", "Cherries", "Mangos", "Berries"]
+    //        myTable.setNumberOfRows(fruits.count, withRowType: "cell")
+    //        var x = 0
+    //        for fruit in fruits {
+    //            let row = myTable.rowController(at: x) as? RowController
+    //            row?.myLabel.setText(fruit)
+    //            x += 1
+    //        }
+    //    }
+
+
+//    func setupInput() {
+//        self.presentTextInputController(withSuggestions: nil, allowedInputMode: .allowEmoji) { (results) in
+//            guard let results = results else {return}
+//            DispatchQueue.main.async {
+//                self.dismissTextInputController()
+//                self.textLabel.setText(results[0] as? String)
+//            }
 //        }
 //    }
-    
-    
 }
+
+
