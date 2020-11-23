274ddee6a46817a2b7ff801bdf42cb3f41355b59
Itunu Raimi <chavitunu@gmail.com>
2020-11-23 09:59:57 +0100

    created input text in apple watch

--- Grocery List Watch Extension/InterfaceController.swift
+++ Grocery List Watch Extension/InterfaceController.swift
@@ -9,18 +9,11 @@ import WatchKit
 import Foundation
 
 class InterfaceController: WKInterfaceController {
-    @IBOutlet var myTable: WKInterfaceTable!
-
+    @IBOutlet var myButton: WKInterfaceButton!
+    @IBOutlet var textLabel: WKInterfaceLabel!
+    
     override func awake(withContext context: Any?) {
-        let fruits = ["Apples", "Oranges", "Cherries", "Mangos", "Berries"]
-        myTable.setNumberOfRows(fruits.count, withRowType: "cell")
-        var x = 0
-        for fruit in fruits {
-            let row = myTable.rowController(at: x) as? RowController
-            row?.myLabel.setText(fruit)
-            x += 1
-        }
-        
+//        setupTableView()
     }
     
     override func willActivate() {
@@ -28,9 +21,29 @@ class InterfaceController: WKInterfaceController {
     }
     
     override func didDeactivate() {
-    
+        
     }
     
-
-
+    @IBAction func buttonTapped() {
+        self.presentTextInputController(withSuggestions: nil, allowedInputMode: .allowEmoji) { (results) in
+            guard let results = results else {return}
+            DispatchQueue.main.async {
+                self.dismissTextInputController()
+                self.textLabel.setText(results[0] as? String)
+            }
+        }
+    }
+    
+//    func setupTableView() {
+//        let fruits = ["Apples", "Oranges", "Cherries", "Mangos", "Berries"]
+//        myTable.setNumberOfRows(fruits.count, withRowType: "cell")
+//        var x = 0
+//        for fruit in fruits {
+//            let row = myTable.rowController(at: x) as? RowController
+//            row?.myLabel.setText(fruit)
+//            x += 1
+//        }
+//    }
+    
+    
 }
